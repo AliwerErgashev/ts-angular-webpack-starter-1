4f31f68f1d77182a35bffe62f9bd386de02db2ab
ravshan <ravshansbox@gmail.com>
2017-11-20 04:57:28 +0500

    remove error handler;

--- src/server/main.ts
+++ src/server/main.ts
@@ -9,9 +9,4 @@ app.use(express.static('public'))
 app.get('/rpc', rpcInfoHandler)
 app.post('/rpc', rpcProcessHandler)
 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
-
-app.listen(9000, () => console.log('HTTP server up on port 9000'))
+app.listen(9000, () => console.info('HTTP server up on port 9000'))
--- src/server/rpc-config.ts
+++ src/server/rpc-config.ts
@@ -2,10 +2,6 @@ import { prefixKeys } from '../common/utils'
 import { registerMethodDefinitionMap } from './rpc'
 import { userApi } from './users/user-api'
 
-const map = {
+registerMethodDefinitionMap({
   ...prefixKeys(userApi, 'users')
-}
-
-console.log(map)
-
-registerMethodDefinitionMap(map)
+})
