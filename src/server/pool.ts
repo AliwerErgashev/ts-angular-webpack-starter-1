102530076432ad3de003f847b4daf6bea5fb81ff
ravshan <ravshansbox@gmail.com>
2017-11-25 02:53:57 +0500

    get DATABASE_URL from environment;

--- src/server/pool.ts
+++ src/server/pool.ts
@@ -1,6 +1,3 @@
-import { readFileSync } from 'fs'
 import { Pool } from 'pg'
 
-const { db } = JSON.parse(readFileSync(`${__dirname}/../../config/default.json`, 'utf8'))
-
-export const pool = new Pool({ connectionString: db })
+export const pool = new Pool({ connectionString: process.env.DATABASE_URL })
