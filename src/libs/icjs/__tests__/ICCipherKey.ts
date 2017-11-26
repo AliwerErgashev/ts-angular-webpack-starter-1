0d1bad8ddde4a83e21e214bb2958276ec0760947
ravshan <ravshansbox@gmail.com>
2017-11-26 23:51:24 +0500

    move dh test to appropriate test suite;

--- src/libs/icjs/__tests__/ICCipherKey.ts
+++ src/libs/icjs/__tests__/ICCipherKey.ts
@@ -9,4 +9,12 @@ describe('ICCipherKey', () => {
     expect(privateKey.toHex()).toHaveLength(64)
     expect(publicKey.toHex()).toHaveLength(64)
   })
+
+  test('dh', () => {
+    const cipherKey1 = new ICCipherKey()
+    const cipherKey2 = new ICCipherKey()
+    const dh1 = ICCipherKey.dh(cipherKey1.getPrivateKey().toHex(), cipherKey2.getPublicKey().toHex())
+    const dh2 = ICCipherKey.dh(cipherKey2.getPrivateKey().toHex(), cipherKey1.getPublicKey().toHex())
+    expect(dh1).toEqual(dh2)
+  })
 })
