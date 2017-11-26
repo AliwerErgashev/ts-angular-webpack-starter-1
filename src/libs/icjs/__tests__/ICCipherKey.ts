0c1c8043892f2c5f934aa091a53f2e8333c55cab
ravshan <ravshansbox@gmail.com>
2017-11-26 21:49:39 +0500

    convert ICCipherKey to test suite;

--- src/libs/icjs/__tests__/ICCipherKey.ts
+++ src/libs/icjs/__tests__/ICCipherKey.ts
@@ -2,9 +2,11 @@ import { ICCipherKey } from '../ICCipherKey'
 
 const cipherKey = new ICCipherKey()
 
-test('ICCipherKey', () => {
-  const privateKey = cipherKey.getPrivateKey()
-  const publicKey = cipherKey.getPublicKey()
-  expect(privateKey.toHex()).toHaveLength(64)
-  expect(publicKey.toHex()).toHaveLength(64)
+describe('ICCipherKey', () => {
+  test('length', () => {
+    const privateKey = cipherKey.getPrivateKey()
+    const publicKey = cipherKey.getPublicKey()
+    expect(privateKey.toHex()).toHaveLength(64)
+    expect(publicKey.toHex()).toHaveLength(64)
+  })
 })
